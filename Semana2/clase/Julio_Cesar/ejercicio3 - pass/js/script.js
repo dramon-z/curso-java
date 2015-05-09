aaa18f924d5e307df11b4dd74f0ed2b3a6bb686e
JULIODAVIDS <juliodavids@hotmail.com>
2015-05-09 16:52:24 +0200

    Julio_Cesar

---
+++ Semana2/clase/Julio_Cesar/ejercicio3 - pass/js/script.js
@@ -0,0 +1,97 @@
+var MiClase;
+MiClase = function(){
+	"use strict";
+	var init,_config
+	,handleStorage
+	,handleEjemplos
+	,eventClick
+	;
+	init = function(config){
+		_config =config;
+		console.log(_config);
+		handleStorage();
+	};
+	eventClick = function(){
+		var _username="admin",
+			_password = "root",
+			username,
+			password;
+
+		username = document.getElementsByName('username')[0].value;
+		password = document.getElementsByName('password')[0].value;
+		if(password !=_password){
+			alert("La contraseña es incorrecta");
+		}else{
+			alert("Bienvenido");
+		}
+	};
+	handleStorage = function(){
+		if(typeof(Storage) !== "undefined") {
+			handleEjemplos();
+		} else {
+			alert("El sitio web no es soportado por su navegador");
+		}
+	};
+	handleEjemplos = function(){
+		var telefonos= {
+		casa:"123214",
+		oficina:"3213424"
+			};
+		var variable = {
+			nombre:"Daniel",
+			ap_paterno:"Ramon",
+			ap_materno:"Zapata",
+			telefonos:telefonos
+		};
+		var json_miObjeto = JSON.stringify(variable);
+		localStorage.setItem("nombre", json_miObjeto);
+		document.getElementById("storage").innerHTML = localStorage.getItem("nombre");
+		console.log( localStorage.getItem("nombre"));
+		var obj = JSON.parse(localStorage.getItem("nombre"));
+		localStorage.removeItem("nombre");
+		console.log(obj);
+	};	
+	return {
+		inicializar:init,
+		eventClick:eventClick
+	};
+};
+
+
+
+
+
+
+/**
+function Objeto(){
+	this.cadena="micadena";
+	this.aleatorio=Math.random();
+};
+
+var mi_funcion = function(str){
+	mi_variable_local = "dos";
+	
+	document.getElementById("ejemplo").innerHTML =str;
+}
+
+mi_funcion("tres");
+console.log(mi_variable_local);
+**/
+
+
+
+//**ver STORAGE
+
+//VALIDAR SI ES SOPORTADO
+if(typeof(Storage) !== "undefined") {
+    // Code for localStorage/sessionStorage.
+} else {
+    // Sorry! No Web Storage support..
+}
+
+// Store
+//localStorage.setItem("nombre", "Smith");
+// Retrieve
+//document.getElementById("result").innerHTML = localStorage.getItem("nombre");
+
+//localStorage.removeItem("el");
