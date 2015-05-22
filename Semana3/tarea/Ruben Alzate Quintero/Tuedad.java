88f61c517d203b1b3919f1b274868de2dd9e1f24
quecosa65 <quecosa65@hotmail.com>
2015-05-22 18:15:09 -0500

    Converti solicitar texto, ints, calcular edad y espacio para formato en Metodos para ser usados en el main.
    Saludos, agradezco sus comentarios.

--- Semana3/tarea/Ruben Alzate Quintero/Tuedad.java
+++ Semana3/tarea/Ruben Alzate Quintero/Tuedad.java
@@ -27,86 +27,37 @@ La siguiente es la lista unicode para que el texto que se imprima en System.out.
 
 
 public class Tuedad{
-	public static void main(String[] args)
-	    {
-	    	
-		for(int clear = 0; clear < 200; clear++) //Simulo limpiar pantalla, aunque en realidad no lo hago :P
-			  {
-			     System.out.println("\b") ;
-			  }
 
+	public static String texto(String elemento) { //Creo el METODO DE LA CLASE que retorna String
+		Scanner sc = new Scanner(System.in); //Creo el objeto scanner
+		String texto="";
+		boolean textoCorrecto;
+		do{
+			System.out.println("Dime tu "+elemento+", solo letras : ");
+			texto = sc.nextLine();
+			textoCorrecto=texto.matches("[a-zA-z ñÑ]*");
+		}while(!textoCorrecto);
+			return texto;
+		}
 
-	    	Scanner sc = new Scanner(System.in); //Creo el objeto scanner
-	    	
-//Solicito string y valido que solo ingrese string de a a la z y de A a la Z (MAYUSCULAS)
+	public static int entero(String elemento, int rango_izquierdo, int rango_derecho) { //Creo el METODO DE LA CLASE que retorna int.
+		Scanner sc = new Scanner(System.in);
+    	int numero=0;
+	    do {
+	        System.out.println("Ingresa el "+elemento+" de nacimiento, debe ser n\u00famero positivo entre "+rango_izquierdo+"-"+rango_derecho+": "); // \u00fa es el unicode para ú
+	        while (!sc.hasNextInt()) {
+	            System.out.println(elemento+" de nacimiento debe ser n\u00famero positivo entre "+rango_izquierdo+"-"+rango_derecho+": ");
+	            sc.next(); // Importante, no se debe de olvidar ingresar el next para que no vuela a cargar el mismo token
+	        }
+	        numero = sc.nextInt();
+	    } while (numero < rango_izquierdo || numero > rango_derecho);
+	    return numero;
+	}
 
-	    	String nombre="";
-	        boolean nombreCorrecto;
-	        do{
-	            System.out.println("Dime tu NOMBRE, solo letras : ");
-	            nombre = sc.nextLine();
-	            nombreCorrecto=nombre.matches("[a-zA-z ñÑ]*");
-	        }while(!nombreCorrecto);
-
-//Solicito string y valido que solo ingrese string de a a la z y de A a la Z (MAYUSCULAS)
-
-	    	String ape_pat="";
-	        boolean ape_patCorrecto;
-	        do{
-	            System.out.println("Dime tu APELLIDO PATERNO, solo letras : ");
-	            ape_pat = sc.next();
-	            ape_patCorrecto=ape_pat.matches("[a-zA-z ñÑ]*");
-	        }while(!ape_patCorrecto);
-
-//Solicito string y valido que solo ingrese string de a a la z y de A a la Z (MAYUSCULAS)
-
-	    	String ape_mat="";
-	        boolean ape_matCorrecto;
-	        do{
-	            System.out.println("Dime tu APELLIDO MATERNO, solo letras : ");
-	            ape_mat = sc.next();
-	            ape_matCorrecto=ape_mat.matches("[a-zA-z ñÑ]*");
-	        }while(!ape_matCorrecto);
-
-//Solicito int y me aseguro que cumpla con: solo positivo, y que ente entre el rango correspondiente
-	    	
-	    	int dia_nac=0;
-		    do {
-		        System.out.println("Ingresa el DIA de nacimiento, debe ser n\u00famero positivo entre 1-31: "); // \u00fa es el unicode para ú
-		        while (!sc.hasNextInt()) {
-		            System.out.println("DIA de nacimiento debe ser n\u00famero positivo entre 1-31: ");
-		            sc.next(); // Importante, no se debe de olvidar ingresar el next para que no vuela a cargar el mismo token
-		        }
-		        dia_nac = sc.nextInt();
-		    } while (dia_nac < 0 || dia_nac > 31);
-	    	
-//Solicito int y me aseguro que cumpla con: solo positivo, y que ente entre el rango correspondiente
-
-	    	int mes_nac=0;
-		    do {
-		        System.out.println("Ingresa el MES de nacimiento, debe ser n\u00famero positivo entre 1-12: ");
-		        while (!sc.hasNextInt()) {
-		            System.out.println("MES de nacimiento debe ser n\u00famero positivo entre 1-12: ");
-		            sc.next(); // Importante, no se debe de olvidar ingresar el next para que no vuela a cargar el mismo token
-		        }
-		        mes_nac = sc.nextInt();
-		    } while (mes_nac < 0 || mes_nac > 12);
-
-//Solicito int y me aseguro que cumpla con: solo positivo, y que ente entre el rango correspondiente
-
-			int anio_nac=0;
-		    do {
-		        System.out.println("Ingresa el A\u00d1O de nacimiento, debe ser n\u00famero positivo entre 1-2015: "); //Uso el unicode \u00d1 para mostrar correctamente la Ñ
-		        while (!sc.hasNextInt()) {
-		            System.out.println("A\u00d1O de nacimiento debe ser n\u00famero positivo entre 1-2015: ");
-		            sc.next(); // Importante, no se debe de olvidar ingresar el next para que no vuela a cargar el mismo token
-		        }
-		        anio_nac = sc.nextInt();
-		    } while (anio_nac < 1 || anio_nac > 2015);
-
+	public static String edad(int dia_nac,int mes_nac,int anio_nac) { //Creo el METODO DE LA CLASE que retorna la edad como un String.
 		    Calendar calendario = Calendar.getInstance(); //Creo el objeto calendario para sacar el dia, el mes y el año
 		    int dia_actual=calendario.get(Calendar.DAY_OF_MONTH);
-		    int mes_actual=calendario.get(Calendar.MONTH)+1; //Agrego +a debido a que calendar.month inicia en 0(enero)-11(diciembre), asi que para igualar el valor le sumo 1.
+		    int mes_actual=calendario.get(Calendar.MONTH)+1; //Agrego +1 debido a que calendar.month inicia en 0(enero)-11(diciembre), asi que para igualar el valor le sumo 1.
 		    int anio_actual=calendario.get(Calendar.YEAR);
 
 		    int iYear = anio_actual - anio_nac; //Hago la diferencia de año actual menos el año de nacimiento
@@ -122,37 +73,51 @@ public class Tuedad{
 		        iMonth = 12 - Math.abs(iMonth);
 		        iYear = iYear - 1;
 		    }
-		    
-			for(int clear = 0; clear < 200; clear++) //Simulo limpiar pantalla, aunque en realidad no lo hago :P
+
+		    String edad = iYear+ " a\u00f1o(s) "+iMonth+" mes(es) "+d+" dia(s)";
+		    return edad;
+
+	}
+
+	public static void espacio(int lineas) { //Creo el METODO DE LA CLASE que avanza la consola n numero de lineas.
+		for(int clear = 0; clear < lineas; clear++) //Subo la tabla anterior 5 espacios
 			  {
 			     System.out.println("\b") ;
 			  }
 
+	}
+
+	public static void main(String[] args)
+	    {
+	    	
+		espacio(200); //Simulo limpiar pantalla
+
+		String nombre = texto("NOMBRE"); //Pido el dato
+		String ape_pat = texto("APELLIDO PATERNO"); //Pido el dato
+		String ape_mat = texto("APELLIDO MATERNO"); //Pido el dato
+		espacio(100); //limpio la pantalla
+		int dia_nac = entero("DIA",0,31); //Pido DIA
+		int mes_nac = entero("MES",0,12); //Pido MES
+		int anio_nac = entero("A\u00d1O",1,2015); //Pido AÑO
+		String edad = edad(dia_nac,mes_nac,anio_nac); // Pido la edad
+		espacio(100); //limpio la pantalla
+		Calendar calendario = Calendar.getInstance(); //Creo el objeto calendario para sacar el dia, el mes y el año actual.
 //Aqui imprimo la tabla de los valores por el cual se formó el resultado para que sea facilmente cotejable.
-			System.out.println("ACTUAL:");
-			System.out.println("\tDIA\tMES\tA\u00d1O");
-			System.out.println("\t"+dia_actual+"\t"+mes_actual+"\t"+anio_actual);
-   			System.out.println("\b") ;
-   			System.out.println("NACIMIENTO:");
-   			System.out.println("\tDIA\tMES\tA\u00d1O");
-			System.out.println("\t"+dia_nac+"\t"+mes_nac+"\t"+anio_nac);
-			System.out.println("\b") ;
-			System.out.println("DIFERENCIA EN:");
-			System.out.println("\tDIAS\tMESES\tA\u00d1OS");
-			System.out.println("\t"+d+"\t"+iMonth+"\t"+iYear);
+		System.out.println("ACTUAL:");
+		System.out.println("\tDIA\tMES\tA\u00d1O");
+		System.out.println("\t"+calendario.get(Calendar.DAY_OF_MONTH)+"\t"+(calendario.get(Calendar.MONTH)+1)+"\t"+calendario.get(Calendar.YEAR));
+		System.out.println("\b") ;
+		System.out.println("NACIMIENTO:");
+		System.out.println("\tDIA\tMES\tA\u00d1O");
+		System.out.println("\t"+dia_nac+"\t"+mes_nac+"\t"+anio_nac);
+		System.out.println("\b");
+		System.out.println("La diferencia es: "+edad+" por lo tanto...");
 
-			for(int clear = 0; clear < 5; clear++) //Subo la tabla anterior 5 espacios
-			  {
-			     System.out.println("\b") ;
-			  }
+		espacio(5); //Subo la pantalla 5 espacios
 
-		    String NOMBRE = new String(nombre+" "+ape_pat+" "+ape_mat); 		    
-		    System.out.println(NOMBRE.toUpperCase()+" tu edad es: "+iYear+ " a\u00f1o(s) "+iMonth+" mes(es) "+d+" dia(s)"); //Uso el unicode \u00f1 para presentar correctamente la ñ
-
-			for(int clear = 0; clear < 5; clear++) //Subo la salida anterior para que se vea bonito
-			  {
-			     System.out.println("\b") ;
-			  }		      
-		    
+	    String NOMBRE = new String(nombre+" "+ape_pat+" "+ape_mat); //Hago el upper para el nombre completo
+	    System.out.println(NOMBRE.toUpperCase()+" tu edad es: "+edad); //Uso el unicode \u00f1 para presentar correctamente la ñ
+	    espacio(5);//le doy 5 espacios para darle formato
+			    
 	    }
 }
