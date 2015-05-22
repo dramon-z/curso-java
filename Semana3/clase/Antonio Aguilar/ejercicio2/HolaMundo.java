344e4098ee806687282e7b4cf68c78313e1f9be9
SinosTeapa <tino198@hotmail.com>
2015-05-22 12:00:18 -0500

    sin cambios
    sin cambios

---
+++ Semana3/clase/Antonio Aguilar/ejercicio2/HolaMundo.java
@@ -0,0 +1,14 @@
+/*
+Este programa imprime un saludo en consola utilizando una
+variable que almacena un nombre.
+*/
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		//Variable que almacena un nombre
+		String nombre = "Jose Luis Landero Noriega";
+
+		System.out.println("¡Hola " + nombre + "!");
+	}
+
+}
---
+++ Semana3/clase/Antonio Aguilar/ejercicio3/AsignacionVariables.java
@@ -0,0 +1,52 @@
+
+public class AsignacionVariables {
+
+	public String frase = "Soy un objeto feliz";
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		System.out.println("1. Valor de 'a': " + a);
+		System.out.println("1. Valor de 'b':" + b);
+		System.out.println("");
+
+		b = a;
+
+		System.out.println("2. Valor de 'a': " + a);
+		System.out.println("2. Valor de 'b':" + b);
+		System.out.println("");
+
+		a = 15;
+
+		System.out.println("3. Valor de 'a': " + a);
+		System.out.println("3. Valor de 'b':" + b);
+		System.out.println("");
+
+		AsignacionVariables c = new AsignacionVariables();
+		AsignacionVariables d = new AsignacionVariables();
+
+		System.out.println("4. Frase de 'c': " + c.frase);
+		System.out.println("4. Frase de 'd': " + d.frase);
+		System.out.println("");
+
+		d.frase = "Soy un objeto mas o menos feliz";
+
+		System.out.println("5. Frase de 'c': " + c.frase);
+		System.out.println("5. Frase de 'd': " + d.frase);
+		System.out.println("");
+
+		c = d;
+
+		System.out.println("6. Frase de 'c': " + c.frase);
+		System.out.println("6. Frase de 'd': " + d.frase);
+		System.out.println("");
+
+		c.frase = "Soy un objeto muy loco";
+
+		System.out.println("7. Frase de 'c': " + c.frase);
+		System.out.println("7. Frase de 'd': " + d.frase);
+		System.out.println("");
+	}
+
+}
