9ed1b63041458c1415a9d26505e99dfc8d4f1f93
José Luis Landero Noriega <sikloidz@gmail.com>
2015-05-23 07:38:46 -0500

    semana4
    semana4

---
+++ Semana4/ejercicio_3/xml/ManejadorArchivo.java
@@ -0,0 +1,56 @@
+package xml;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.*;
+
+public class ManejadorArchivo {
+
+	private static final String RUTA_ARCHIVO = "c:\\users\\joselandero\\documents\\base_datos.xml";
+
+	public ManejadorArchivo() {
+		verificarExistencia();
+	}
+
+	public void verificarExistencia() {
+		File archivo = new File(RUTA_ARCHIVO);
+
+		if (!archivo.exists()) {
+			try {
+				crearArchivo();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+	}
+
+	public void crearArchivo() throws ParserConfigurationException, TransformerConfigurationException, FileNotFoundException, TransformerException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.newDocument();
+
+		Element element = document.createElement("Asegurados");
+		document.appendChild(element);
+
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+		Source source = new DOMSource(document);
+		Result result = new StreamResult(new FileOutputStream(RUTA_ARCHIVO));
+		transformer.transform(source, result);
+
+		System.out.print("El archivo de base de datos no existía y lo creamos, por favor presione enter para continuar");
+	}
+
+}
